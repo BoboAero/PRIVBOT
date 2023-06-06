68ff6134894817073842c871940d48a8d326c7a8
BoboAero <wulemsteun@gmail.com>
2023-06-06 20:30:04 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const { Client, IntentsBitField} = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`[index.js] ✅ ${c.user.tag} is online.`)
+})
+
+
+client.login(process.env.TOKEN);
+
+
